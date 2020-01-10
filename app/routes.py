6af46b691be222b4bcd1e50cacdddef4e3a373ce
Leak Master <leakmaster@leakyleaks.lk>
2020-01-11 00:30:24 +0100

    Prepare for forms, according to tutorial
    By the way, this is a neat tutorial. I am sure to have a secure application
    https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -20,3 +21,13 @@ def leaks():
                     },
                 ]
         return render_template("leaks.html", leaks=leaks)
+
+@app.route("/admin")
+@app.route("/admin/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template("login.html", form=form)
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+class Config(object):
+        SECRET_KEY = os.environ.get('SECRET_KEY') or 'Eezai4yairoow5fohZ3vaSh7tu7shae7ie'
