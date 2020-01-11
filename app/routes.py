07cecaf6ac3f60c8f1767c730d6139cc37310136
Leak Master <leakmaster@leakyleaks.lk>
2020-01-11 21:55:27 +0100

    Implement first database functionality
    - Uses local SQLite storage
    - Implements user login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -23,11 +26,27 @@ def leaks():
         return render_template("leaks.html", leaks=leaks)
 
 @app.route("/admin")
+@app.route("/admin/dashboard")
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
 @app.route("/admin/login", methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template("login.html", form=form)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+@app.route("/admin/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'Eezai4yairoow5fohZ3vaSh7tu7shae7ie'
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'leaks.db')
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
